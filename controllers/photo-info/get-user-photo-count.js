cea24c044671de0d1566393f59b7845fd97002cd
Tim Engle <timothy.englew@gmail.com>
2023-11-01 21:19:52 -0700

    📸 Updated photo-info controller to use new model

--- controllers/photo-info/get-user-photo-count.js
+++ controllers/photo-info/get-user-photo-count.js
@@ -1,49 +1,37 @@
 import { fileURLToPath } from 'url';
 import Joi from 'joi';
 
-import { getCatCountCollection } from '../../DB/collections.js';
+import CategoryCount from '../../models/CategoryCount.js';
 import logError from '../../Errors/log-error.js';
-
-const catCountCollection = getCatCountCollection;
 
 const __filename = fileURLToPath(import.meta.url);
 
-const paramSchema = Joi.string().required();
+const paramSchema = Joi.object({
+  userId: Joi.string().length(24).required(),
+});
 
 /**
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
 const getUserPhotoCount = async (req, res) => {
-  let { username } = req.params;
-  username = username.toLowerCase();
+  const { userId } = req.params;
 
   try {
-    const { error } = paramSchema.validate(username);
+    const { error } = paramSchema.validate({ userId });
     if (error) {
       return res
         .status(422)
         .send({ message: 'Validation Error', error: error.details[0].message });
     }
-    console.log('USERNAME: ', username);
-    const userPhotoDoc = await catCountCollection.findOne(
-      { username },
-      {
-        projection: {
-          userId: 1,
-          displayUsername: 1,
-          username: 1,
-          pictureData: 1,
-          totalUploads: 1,
-        },
-      },
-    );
 
-    if (!userPhotoDoc) {
+    const userCategoryCountDoc = await CategoryCount.findByUserId(userId);
+
+    if (!userCategoryCountDoc) {
       return res.status(404).send({ message: 'User not found.' });
     }
 
-    return res.status(200).send(userPhotoDoc);
+    return res.status(200).send(userCategoryCountDoc);
   } catch (error) {
     logError(error, __filename, 'register');
     console.log(error);
--- routes/photo.js
+++ routes/photo.js
@@ -1,12 +1,11 @@
 import express from 'express';
 
 import photoController from '../controllers/photo-upload/index.js';
-import getUserPhotoCount from '../controllers/photo-upload/get-user-photo-count.js';
 
 const photoRoutes = express.Router();
 
 photoRoutes.post('/', photoController.postPhoto);
 
-photoRoutes.get('/:username', getUserPhotoCount);
+photoRoutes.get('/:userId', photoController.getUserPhotoCount);
 
 export default photoRoutes;
