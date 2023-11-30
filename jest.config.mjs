89e2904dab1810dd2d148958fe4ab8287e6a017c
Tim Engle <timothy.englew@gmail.com>
2023-11-29 23:13:34 -0800

    🧪Updated Jest Test env

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,7 @@
 export default {
     testEnvironment: 'node',
     transform: {},
+    verbose: true,
     collectCoverage: true,
     coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
     coverageDirectory: './coverage',
