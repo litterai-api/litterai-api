6f0f70e1bfb238758dafcef39e4ae2c7447daf71
Tim Engle <timothy.englew@gmail.com>
2023-11-30 09:13:37 -0800

    🧪📸100% coverage for PhotoInfo Model

--- test/Models/PhotoInfo.test.js
+++ test/Models/PhotoInfo.test.js
@@ -1,6 +1,10 @@
-import { MongoClient } from 'mongodb';
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-plusplus */
+import { MongoClient, Collection } from 'mongodb';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { faker } from '@faker-js/faker';
+import { jest } from '@jest/globals';
+
 import PhotoInfo from '../../models/PhotoInfo.js';
 import CategoryCount from '../../models/CategoryCount.js';
 import User from '../../models/User.js';
@@ -22,14 +26,39 @@ const getRandomCategory = () => {
     return categories[randomNumber];
 };
 
+const mocks = {
+    throwError: {
+        findOne: () =>
+            jest
+                .spyOn(Collection.prototype, 'findOne')
+                .mockImplementation(() => {
+                    throw new Error('Simulated Error');
+                }),
+        find: () =>
+            jest.spyOn(Collection.prototype, 'find').mockImplementation(() => {
+                throw new Error('Simulated Error');
+            }),
+        insertOne: () =>
+            jest
+                .spyOn(Collection.prototype, 'insertOne')
+                .mockImplementation(() => {
+                    throw new Error('Simulated Error');
+                }),
+
+        deleteMany: () =>
+            jest
+                .spyOn(Collection.prototype, 'deleteMany')
+                .mockImplementation(() => {
+                    throw new Error('Simulated Error');
+                }),
+    },
+};
+
 describe('PhotoInfo Model', () => {
     let mongoServer;
     let client;
     let db;
     let newUser;
-    let newPhotoData;
-    let category;
-    let categoryCountDocument;
 
     beforeAll(async () => {
         mongoServer = await MongoMemoryServer.create();
@@ -51,8 +80,6 @@ describe('PhotoInfo Model', () => {
             faker.person.lastName(),
             faker.location.zipCode('#####'),
         );
-
-        category = getRandomCategory();
     });
 
     afterAll(async () => {
@@ -62,35 +89,46 @@ describe('PhotoInfo Model', () => {
         await mongoServer.stop();
     });
 
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     describe('insertOne', () => {
-        test('should insert photo info document successfully', async () => {
-            newPhotoData = await PhotoInfo.insertOne(category, {
+        const sut = PhotoInfo.insertOne;
+        const category = getRandomCategory();
+        let newPhotoCategoryCount = 0;
+
+        it('should not return null', async () => {
+            const actual = await sut(category, {
                 _id: newUser._id,
                 username: newUser.userName,
             });
-            expect(newPhotoData).not.toBeNull();
+            newPhotoCategoryCount++;
+            expect(actual).not.toBeNull();
         });
 
-        test('should return the same category that was inserted', () => {
-            expect(newPhotoData.category).toEqual(category);
+        it('should return the same category that was inserted', async () => {
+            const actual = await sut(category, {
+                _id: newUser._id,
+                username: newUser.userName,
+            });
+            newPhotoCategoryCount++;
+            expect(actual.category).toEqual(category);
         });
 
-        test("should have a categoryUpload value that is equal to the CategoryCount document's value", async () => {
-            categoryCountDocument = await CategoryCount.findByUserId(
-                newUser._id,
-            );
-            expect(categoryCountDocument.pictureData[category]).toEqual(
-                newPhotoData.categoryUploads,
-            );
+        it("should have a categoryUpload value that is equal to the CategoryCount document's value", async () => {
+            const actual = await CategoryCount.findByUserId(newUser._id);
+            const expected = newPhotoCategoryCount;
+            expect(actual.pictureData[category]).toEqual(expected);
         });
 
-        test("should have a totalUploads value that is equal to the CategoryCount document's value", () => {
-            expect(categoryCountDocument.totalUploads).toEqual(
-                newPhotoData.totalUploads,
-            );
+        it("should have a totalUploads value that is equal to the CategoryCount document's value", async () => {
+            const actual = await CategoryCount.findByUserId(newUser._id);
+            const expected = newPhotoCategoryCount;
+            expect(actual.totalUploads).toEqual(expected);
         });
 
-        test('should throw an error if an invalid userId is entered', async () => {
+        it('should throw an error if an invalid userId is entered', async () => {
             await expect(
                 PhotoInfo.insertOne(category, {
                     _id: 22,
@@ -98,39 +136,102 @@ describe('PhotoInfo Model', () => {
                 }),
             ).rejects.toThrow("Unable to locate user's category");
         });
+        it('should throw an error if one occurs while querying the database', async () => {
+            mocks.throwError.insertOne();
+            let didNotThrow = false;
+            try {
+                await sut(category, {
+                    _id: newUser._id,
+                    username: newUser.userName,
+                });
+                didNotThrow = true;
+            } catch (error) {
+                expect(error.message).toContain('Simulated Error');
+                expect(error.statusCode).toBe(500);
+            }
+
+            if (didNotThrow) {
+                throw new Error(
+                    'Expected function to throw an Error, but it did not throw',
+                );
+            }
+        });
     });
 
     describe('getAllUsersPhotoInfo', () => {
-        let photoArray;
+        const sut = PhotoInfo.getAllUsersPhotoInfo;
+        const newUserPhotoArrayLength = 2;
 
-        beforeAll(async () => {
-            photoArray = await PhotoInfo.getAllUsersPhotoInfo(newUser._id);
+        it('should return an array', async () => {
+            let actual = await sut(newUser._id);
+            expect(actual).toBeInstanceOf(Array);
+            actual = await PhotoInfo.getAllUsersPhotoInfo(22);
+            expect(actual).toBeInstanceOf(Array);
         });
 
-        test('should return an array', async () => {
-            expect(photoArray).toBeInstanceOf(Array);
-            const emptyPhotoArray = await PhotoInfo.getAllUsersPhotoInfo(22);
-            expect(emptyPhotoArray).toBeInstanceOf(Array);
+        it('should return the correct length', async () => {
+            const actual = await sut(newUser._id);
+            expect(actual).toHaveLength(newUserPhotoArrayLength);
         });
 
-        test('should return the correct length', () => {
-            expect(photoArray.length).toEqual(1);
+        it('should be contain the correct object', async () => {
+            const actual = await sut(newUser._id);
+            actual.forEach((photoData) => {
+                expect(photoData).toHaveProperty('_id');
+                expect(photoData).toHaveProperty('userId');
+                expect(photoData).toHaveProperty('category');
+                expect(photoData).toHaveProperty('createdAt');
+            });
         });
 
-        test('should be contain the correct object', () => {
-            expect(photoArray[0]).toHaveProperty('_id');
-            expect(photoArray[0]).toHaveProperty('userId');
-            expect(photoArray[0]).toHaveProperty('category');
-            expect(photoArray[0]).toHaveProperty('createdAt');
-        });
+        it('should throw an error if one occurs while querying the database', async () => {
+            mocks.throwError.find();
+            let didNotThrow = false;
+            try {
+                await sut(newUser._id);
+                didNotThrow = true;
+            } catch (error) {
+                expect(error.message).toContain('Simulated Error');
+                expect(error.statusCode).toBe(500);
+            }
 
-        test('should throw errors with a statusCode property of 500', async () => {
-            await expect(
-                PhotoInfo.getAllUsersPhotoInfo('id'),
-            ).rejects.toThrow();
-            await expect(
-                PhotoInfo.getAllUsersPhotoInfo('id'),
-            ).rejects.toHaveProperty('statusCode', 500);
+            if (didNotThrow) {
+                throw new Error(
+                    'Expected function to throw an Error, but it did not throw',
+                );
+            }
+        });
+    });
+    describe('deleteSingleUsersInfo', () => {
+        const sut = PhotoInfo.deleteSingleUsersInfo;
+        it('should delete all documents in collection for provided userId', async () => {
+            const actualBeforeDelete = await PhotoInfo.getAllUsersPhotoInfo(
+                newUser._id,
+            );
+            expect(actualBeforeDelete.length).toBeGreaterThan(0);
+            // Delete documents
+            await sut(newUser._id);
+            const actualAfterDelete = await PhotoInfo.getAllUsersPhotoInfo(
+                newUser._id,
+            );
+            expect(actualAfterDelete).toHaveLength(0);
+        });
+        it('should throw an error if one occurs while querying the database', async () => {
+            mocks.throwError.deleteMany();
+            let didNotThrow = false;
+            try {
+                await sut(newUser._id);
+                didNotThrow = true;
+            } catch (error) {
+                expect(error.message).toContain('Simulated Error');
+                expect(error.statusCode).toBe(500);
+            }
+
+            if (didNotThrow) {
+                throw new Error(
+                    'Expected function to throw an Error, but it did not throw',
+                );
+            }
         });
     });
 });
